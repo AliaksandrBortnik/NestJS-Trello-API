61ab4bbb8ca9ba197db01cb852185cee77744aeb
Alex Bortnik <bortnik.ar@gmail.com>
2022-02-05 00:14:04 +0300

    Toggle underlying framework

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {NestExpressApplication} from "@nestjs/platform-express";
 import {config} from "./common/config";
+import {FastifyAdapter, NestFastifyApplication} from "@nestjs/platform-fastify";
 
 const PORT: string = config.PORT || '4000';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  // const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  // const app = await NestFactory.create<NestFastifyApplication>(AppModule);
-  await app.listen(PORT);
+  // https://docs.nestjs.com/techniques/performance
+  const app = config.USE_FASTIFY ?
+    await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter()) :
+    await NestFactory.create(AppModule);
+
+  await app.listen(PORT, '0.0.0.0'); // TODO: review. is it fine for express 0.0.0.0?
+  // TODO: log errors on listen
+  // app.listen(PORT, '0.0.0.0').catch((error: unknown) => {
+  //   if (error instanceof Error) {
+  //     app.log.fatal(error.message);
+  //     process.exit(1);
+  //   }
+  // });
 }
 bootstrap();
