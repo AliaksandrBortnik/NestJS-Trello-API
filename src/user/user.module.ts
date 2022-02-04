a1672e1968d17e9f31708ae4acf39edc2baa3f21
Alex Bortnik <bortnik.ar@gmail.com>
2022-02-05 02:08:51 +0300

    Minor refactoring for User module providers

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,17 +2,12 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import {userProviders} from "./user.providers";
-import {UserEntity} from "./entities/user.entity";
 import {DatabaseModule} from "../database/database.module";
 
 @Module({
-  // imports: [TypeOrmModule.forFeature([UserEntity])]],
   imports: [DatabaseModule],
   exports: [UserService],
   controllers: [UserController],
-  providers: [
-    ...userProviders,
-    UserService
-  ]
+  providers: [...userProviders]
 })
 export class UserModule {}
--- src/user/user.providers.ts
+++ src/user/user.providers.ts
@@ -1,10 +1,12 @@
 import {Connection} from "typeorm";
 import {UserRepository} from "./user.repository";
+import {UserService} from "./user.service";
 
 export const userProviders = [
   {
     provide: 'USER_REPOSITORY',
     useFactory: (connection: Connection) => connection.getCustomRepository(UserRepository),
     inject: ['DATABASE_CONNECTION']
-  }
+  },
+  UserService
 ];
