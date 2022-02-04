cef9a0a3bb0fa728442c4e723c7c0683414899f7
Alex Bortnik <bortnik.ar@gmail.com>
2022-02-04 12:35:30 +0300

    Leverage .env file to use PORT value to bootstrap app

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {NestExpressApplication} from "@nestjs/platform-express";
+import {config} from "./common/config";
+
+const PORT: string = config.PORT || '4000';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  // const app = await NestFactory.create<NestFastifyApplication>(AppModule);
+  await app.listen(PORT);
 }
 bootstrap();
