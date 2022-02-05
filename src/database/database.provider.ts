f27f7ae821167a2fd0dfaf9cbb344cf8e5b09ebb
Alex Bortnik <bortnik.ar@gmail.com>
2022-02-05 12:54:54 +0300

    Minor clean up for database provider file

--- src/database/database.provider.ts
+++ src/database/database.provider.ts
@@ -2,18 +2,5 @@ import { createConnection } from 'typeorm';
 
 export const databaseProvider = {
   provide: 'DATABASE_CONNECTION',
-  useFactory: async () => await createConnection(
-  //   {
-  //   type: 'mysql',
-  //   host: 'localhost',
-  //   port: 3306,
-  //   username: 'root',
-  //   password: 'root',
-  //   database: 'test',
-  //   entities: [
-  //     __dirname + '/../**/*.entity{.ts,.js}',
-  //   ],
-  //   synchronize: true, // TODO: change to false
-  // }
-  ),
+  useFactory: async () => await createConnection()
 };
